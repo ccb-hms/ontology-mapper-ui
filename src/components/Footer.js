6d13f09e3ba0f9f988c1dfea3a2c2a7bccc80515
Jason Payne <paynejason@gmail.com>
2022-12-13 15:13:56 -0500

    Added Hyperlinks
    Added hyperlinks to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ function Footer() {
 	return (
 		<div className="footer">
 			<br />
-			<span>UI Version 1.0.0 &#183; </span>
-			<span>Text2term Version 1.1.0</span>
+			<a href="https://github.com/ccb-hms/ontology-mapper-ui">UI Version 1.0.0 &#183; </a>
+			<a href="https://github.com/ccb-hms/ontology-mapper">Text2term Version 1.1.0</a>
 			<br />
-			<span>&copy; Center for Computational Biomedicine at Harvard Medical School 2022</span>
+			<a href="https://computationalbiomed.hms.harvard.edu/">&copy; Center for Computational Biomedicine at Harvard Medical School 2022</a>
 		</div>
 	);
 }
