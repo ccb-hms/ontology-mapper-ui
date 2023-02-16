1d985b77dd73a6f41b33f351aa0673fdcf32104c
Jason Payne <paynejason@gmail.com>
2023-02-16 10:43:55 -0500

    Version Specification
    Minor edits to specify text2term version for server use.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,9 @@ function Footer() {
 	return (
 		<div className="footer">
 			<br />
-			<span>UI Version 1.0.0 &#183; </span>
-			<span>Text2term Version 1.1.0</span>
+			<span>UI Version 1.0.0 </span>
+			<span>&#183; </span>
+			<span>Text2term Version 2.0.0</span>
 			<br />
 			<span>&copy; Center for Computational Biomedicine at Harvard Medical School 2022</span>
 		</div>
